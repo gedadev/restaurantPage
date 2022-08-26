0612763ff67ba17139032a4f1de633e9d9f3e32c
gedadev <gjbg1234@gmail.com>
2022-08-25 22:54:23 -0500

    Added the menu items to the navbar

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ import './style.css';
 import Logo from './kklogo.png'
 
 function navbar() {
-    const element = document.createElement('div');
-    element.id = 'navbar';
+    const container = document.createElement('div');
+    container.id = 'navbar';
 
     const logo = new Image();
     logo.src = Logo;
     logo.id = 'logo';
-    element.appendChild(logo);
+    container.appendChild(logo);
+
+    let menu = ['Home', 'Galley Grub', 'Info'];
+    menu.forEach((element) => {
+        const menuDiv = document.createElement('div');
+        menuDiv.className = 'menu-item';
+        menuDiv.innerHTML = element;
+        container.appendChild(menuDiv);
+    });
   
-    return element;
+    return container;
 }
   
 document.body.appendChild(navbar());
